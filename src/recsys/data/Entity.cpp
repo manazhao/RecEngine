f6772208db60fc07480d0667c5b519eaeb5b92d4
manazhao <manazhao@gmail.com>
2014-03-31 00:00:20 -0700

    allows to store entity in main memory

--- src/recsys/data/Entity.cpp
+++ src/recsys/data/Entity.cpp
@@ -15,38 +15,39 @@ using namespace boost;
 
 namespace recsys {
 
-Entity::type_name_map Entity::initTypeNameMap() {
+Entity::type_name_map Entity::init_type_name_map() {
 	type_name_map typeNameMap;
 	typeNameMap[ENT_USER] = "user";
 	typeNameMap[ENT_ITEM] = "item";
 	return typeNameMap;
 }
 
-Entity::type_name_map Entity::m_typeNameMap = Entity::initTypeNameMap();
+Entity::type_name_map Entity::m_typeNameMap = Entity::init_type_name_map();
 
-Entity::SharedData Entity::initSharedData() {
+Entity::SharedData Entity::init_shared_data() {
 	static bool inited = false;
 	if (!inited) {
 		/// create the entity table
 		SQL& SQL_INST = SQL::ref();
-		string createTbSql =
-				"CREATE TABLE IF NOT EXISTS entity (id VARCHAR(255) NOT NULL, mapped_id  INT NOT NULL, type TINYINT(1) NOT NULL, value TEXT, INDEX (mapped_id), PRIMARY KEY(id,type) )";
+		string
+				createTbSql =
+						"CREATE TABLE IF NOT EXISTS entity (id VARCHAR(255) NOT NULL, mapped_id  INT NOT NULL, type TINYINT(1) NOT NULL, value TEXT, INDEX (mapped_id), PRIMARY KEY(id,type) )";
 		SQL_INST.m_statement->execute(createTbSql);
 		/// create the PreparedStatement for entity insertion
 		SharedData sharedData;
-		sharedData.m_insertStmtPtr =
-				prepared_statement_ptr(
+		sharedData.m_insertStmtPtr
+				= prepared_statement_ptr(
 						SQL_INST.m_connection->prepareStatement(
 								"INSERT INTO entity (id,mapped_id,type,value) VALUES (?,?,?,?)"));
 		sharedData.m_queryStmtPtr = prepared_statement_ptr(
 				SQL_INST.m_connection->prepareStatement(
 						"SELECT * FROM entity where id = ? AND type = ?"));
-		sharedData.m_updateStmtPtr =
-				prepared_statement_ptr(
+		sharedData.m_updateStmtPtr
+				= prepared_statement_ptr(
 						SQL_INST.m_connection->prepareStatement(
 								"UPDATE entity SET mapped_id = ?, value = ? WHERE id = ? AND type= ?"));
-		sharedData.m_maxIdStmtPtr =
-				prepared_statement_ptr(
+		sharedData.m_maxIdStmtPtr
+				= prepared_statement_ptr(
 						SQL_INST.m_connection->prepareStatement(
 								"SELECT MAX(mapped_id) AS max_id FROM entity WHERE type = ?"));
 
@@ -56,169 +57,140 @@ Entity::SharedData Entity::initSharedData() {
 	return Entity::m_sharedData;
 }
 
-Entity::SharedData Entity::m_sharedData = Entity::initSharedData();
+Entity::SharedData Entity::m_sharedData = Entity::init_shared_data();
+
+Entity::entity_ptr Entity::query_by_nameAndType(string const& name, ushort const& type, bool memoryMode = true){
+	Entity ent(name,type,memoryMode);
+	if(ent.exist()){
+		ent.read();
+		if(memoryMode){
+			return m_type_entity_map[type][ent.m_mapped_id];
+		}
+		/// we have to create one
+		return entity_ptr(new Entity(this));
+	}
+	/// null pointer returned
+	return entity_ptr();
+}
+
+string json_to_string(js::Object& jsObj){
+	/// encode the json object to string
+	stringstream ss;
+	js::Writer::Write(jsObj,ss);
+	return ss.str();
+}
+
+js::Object string_to_json(string const& jsonStr){
+	stringstream ss;
+	ss << jsonStr;
+	js::Object obj;
+	js::Reader::Read(obj,jsonStr);
+	return obj;
+}
+
 
 unsigned int Entity::_get_max_mapped_id(bool &isNull) {
-	prepared_statement_ptr& maxIdQueryStmtPtr =
-			Entity::m_sharedData.m_maxIdStmtPtr;
-	maxIdQueryStmtPtr->setInt(1, m_type);
-	auto_ptr<ResultSet> rs(maxIdQueryStmtPtr->executeQuery());
-	rs->next();
-	isNull = rs->isNull("max_id");
-	return rs->getInt("max_id");
-}
-
-bool Entity::exist() {
-	prepared_statement_ptr& queryStmtPtr = Entity::m_sharedData.m_queryStmtPtr;
-	queryStmtPtr->setString(1, m_id);
-	queryStmtPtr->setInt(2, m_type);
-	auto_ptr<ResultSet> rs(queryStmtPtr->executeQuery());
-	return rs->next();
-}
-
-void Entity::read() {
-	prepared_statement_ptr& queryStmtPtr = Entity::m_sharedData.m_queryStmtPtr;
-	queryStmtPtr->setString(1, m_id);
-	queryStmtPtr->setInt(2, m_type);
-	auto_ptr<ResultSet> rs(queryStmtPtr->executeQuery());
-	if (rs->next()) {
-		/// get the mapped id
-		m_mapped_id = rs->getInt("mapped_id");
-		string kvMapStr = rs->getString("value");
-		/// recover as pairs
-		vector<string> kvPairs;
-		split(kvPairs, kvMapStr, is_any_of("|"));
-		for (size_t i = 0; i < kvPairs.size(); i++) {
-			string kvStr = kvPairs[i];
-			vector<string> kvs;
-			split(kvs, kvStr, is_any_of("-"));
-			assert(kvs.size() == 2);
-			size_t key = lexical_cast<size_t>(kvs[0]);
-			string value = kvs[1];
-			m_kvMap[key] = value;
+	if(m_memory_mode){
+		if(m_type_max_id_map.find(m_type) == m_type_max_id_map.end()){
+			isNull = true;
+			return 0;
+		}else{
+			return m_type_max_id_map[m_type];
 		}
+	}else{
+		prepared_statement_ptr& maxIdQueryStmtPtr =
+				Entity::m_sharedData.m_maxIdStmtPtr;
+		maxIdQueryStmtPtr->setInt(1, m_type);
+		auto_ptr<ResultSet> rs(maxIdQueryStmtPtr->executeQuery());
+		rs->next();
+		isNull = rs->isNull("max_id");
+		return rs->getInt("max_id");
 	}
 }
 
-size_t Entity::attachFeature(string const& name, size_t const& type,
-		string const& value) {
-	/// first check the existence of the feature
-	Feature feat(name, type);
-	feat.write();
-	feat.read();
-	setKV(feat.getId(), value);
-	return feat.getId();
+bool Entity::exist() {
+	if (!m_memory_mode) {
+		prepared_statement_ptr& queryStmtPtr =
+				Entity::m_sharedData.m_queryStmtPtr;
+		queryStmtPtr->setString(1, m_id);
+		queryStmtPtr->setInt(2, m_type);
+		auto_ptr<ResultSet> rs(queryStmtPtr->executeQuery());
+		return rs->next();
+	} else {
+		return !(m_type_name_id_map[m_type].find(m_id)
+				== m_type_name_id_map.end());
+	}
+}
+
+void Entity::read() {
+	string jsonStr;
+	if(!m_memory_mode){
+		prepared_statement_ptr& queryStmtPtr = Entity::m_sharedData.m_queryStmtPtr;
+		queryStmtPtr->setString(1, m_id);
+		queryStmtPtr->setInt(2, m_type);
+		auto_ptr<ResultSet> rs(queryStmtPtr->executeQuery());
+		if (rs->next()) {
+			/// get the mapped id
+			m_mapped_id = rs->getInt("mapped_id");
+			jsonStr = rs->getString("value");
+		}
+	}else{
+		/// try to retrieve from the map
+		if(m_type_name_id_map[m_type].find(m_id) != m_type_name_id_map.end()){
+			m_mapped_id = m_type_name_id_map[m_type][m_id];
+			/// retrieve the json string from the entity map
+			jsonStr = m_type_entity_map[m_type].find(m_mapped_id);
+		}
+	}
+	if(!jsonStr.empty()){
+		m_json_value = string_to_json(jsonStr);
+	}
 }
 
 void Entity::write() {
-	/// get the value of the entity
-	string entityValue = getEntityValue();
 	if (exist()) {
 		/// just update an existing entity
 		read();
-		prepared_statement_ptr& updateStmtPtr =
-				Entity::m_sharedData.m_updateStmtPtr;
-		updateStmtPtr->setInt(1, m_mapped_id);
-		updateStmtPtr->setString(2, entityValue);
-		updateStmtPtr->setString(3, m_id);
-		updateStmtPtr->setInt(4, m_type);
-		updateStmtPtr->executeUpdate();
+		if(m_memory_mode){
+			/// replace the entity
+			m_type_entity_map[m_type][m_mapped_id] = shared_ptr<Entity>(this);
+		}else{
+			prepared_statement_ptr& updateStmtPtr =
+					Entity::m_sharedData.m_updateStmtPtr;
+			updateStmtPtr->setInt(1, m_mapped_id);
+			updateStmtPtr->setString(2, jsonStr);
+			updateStmtPtr->setString(3, m_id);
+			updateStmtPtr->setInt(4, m_type);
+			updateStmtPtr->executeUpdate();
+		}
 
 	} else {
-		prepared_statement_ptr& insertStmtPtr =
-				Entity::m_sharedData.m_insertStmtPtr;
 		bool isNull;
 		m_mapped_id = _get_max_mapped_id(isNull);
 		if(!isNull){
 			m_mapped_id++;
 		}
-		insertStmtPtr->setString(1, m_id);
-		insertStmtPtr->setInt(2, m_mapped_id);
-		insertStmtPtr->setInt(3, m_type);
-		insertStmtPtr->setString(4, entityValue);
-		insertStmtPtr->execute();
-	}
-}
-
-string Entity::getEntityValue() const {
-	// generate string out of the map
-	std::stringstream ss;
-	for (map<size_t, string>::const_iterator iter = m_kvMap.begin();
-			iter != m_kvMap.end(); ++iter) {
-		/// separate the key and value by special character '-'
-		ss << (iter == m_kvMap.begin() ? "" : "|") << iter->first << "-"
-				<< iter->second;
-	}
-	return ss.str();
-}
-
-Feature::SharedData Feature::initSharedData() {
-	static bool inited = false;
-	if (!inited) {
-		/// create the table
-		SQL& SQL_INST = SQL::ref();
-		string createTbSql =
-				"CREATE TABLE IF NOT EXISTS feature(id INT(4) AUTO_INCREMENT NOT NULL, name VARCHAR(128), type TINYINT(1) NOT NULL, PRIMARY KEY(id) )";
-		assert(SQL_INST.execute(createTbSql));
-		/// create the prepared statement as well
-		SharedData sharedData;
-		sharedData.m_insertStmtPtr = prepared_statement_ptr(
-				SQL_INST.m_connection->prepareStatement(
-						"INSERT INTO feature(name,type) VALUES(?,?)"));
-		sharedData.m_queryByNameStmtPtr = prepared_statement_ptr(
-				SQL_INST.m_connection->prepareStatement(
-						"SELECT * from feature WHERE name = ?"));
-		inited = true;
-		return sharedData;
-	}
-	return Feature::m_sharedSqlData;
-}
-
-Feature::SharedData Feature::m_sharedSqlData = Feature::initSharedData();
-
-bool Feature::exist() {
-	prepared_statement_ptr& queryStmtPtr =
-			Feature::m_sharedSqlData.m_queryByNameStmtPtr;
-	queryStmtPtr->setString(1, m_name);
-	auto_ptr<ResultSet> rs(queryStmtPtr->executeQuery());
-	return rs->next();
-}
-
-void Feature::read() {
-	prepared_statement_ptr& queryStmtPtr =
-			Feature::m_sharedSqlData.m_queryByNameStmtPtr;
-	queryStmtPtr->setString(1, m_name);
-	auto_ptr<ResultSet> rs(queryStmtPtr->executeQuery());
-	if (rs->next()) {
-		m_id = rs->getInt(1);
-		m_type = rs->getInt(2);
-	}
-}
-
-void Feature::write() {
-	if (!exist()) {
-		Feature::m_sharedSqlData.m_insertStmtPtr->setString(1, m_name);
-		Feature::m_sharedSqlData.m_insertStmtPtr->setInt(2, m_type);
-		try {
-			Feature::m_sharedSqlData.m_insertStmtPtr->execute();
-		} catch (SQLException& e) {
-			cerr << "error in inserting Feature object: " << e.what() << endl;
+		if(m_memory_mode){
+			m_type_name_id_map[m_id] = m_mapped_id;
+			m_type_id_name_map[m_mapped_id] = m_id;
+			m_type_entity_map[m_mapped_id] = shared_ptr<Entity>(new Entity(*this));
+		}else{
+			prepared_statement_ptr& insertStmtPtr =
+					Entity::m_sharedData.m_insertStmtPtr;
+			insertStmtPtr->setString(1, m_id);
+			insertStmtPtr->setInt(2, m_mapped_id);
+			insertStmtPtr->setInt(3, m_type);
+			insertStmtPtr->setString(4, json_to_string(m_json_value));
+			insertStmtPtr->execute();
 		}
-	} else {
-		cout << "feature - [" << m_name << "] exists, abort the insertion"
-				<< endl;
 	}
 }
 
 ostream& operator<<(ostream& oss, Entity const& entity) {
-	oss << "id:" << entity.m_id << ",value:" << entity.getEntityValue() << endl;
+	oss << "id:" << entity.m_id << ",value:" << json_to_string(entity.m_json_value) << endl;
 	return oss;
 }
 
-void test_feature() {
-	Feature feat("gender", Feature::CATEGORY);
-	feat.write();
-}
 
 void test_entity() {
 	Entity entity("zq", Entity::ENT_USER);
@@ -227,25 +199,23 @@ void test_entity() {
 	entity.read();
 	entity.attachFeature("occupation", Feature::CATEGORY, "student");
 	entity.write();
-	entity = Entity("someone",Entity::ENT_USER);
-	entity.attachFeature("age",Feature::NUMERICAL,"20");
-	entity.attachFeature("gender",Feature::CATEGORY,"0");
-	entity.attachFeature("occupation",Feature::CATEGORY,"engineer");
+	entity = Entity("someone", Entity::ENT_USER);
+	entity.attachFeature("age", Feature::NUMERICAL, "20");
+	entity.attachFeature("gender", Feature::CATEGORY, "0");
+	entity.attachFeature("occupation", Feature::CATEGORY, "engineer");
 	entity.write();
-	entity = Entity("xbox",Entity::ENT_ITEM);
-	entity.attachFeature("merchant",Feature::CATEGORY,"ms");
-	entity.attachFeature("price",Feature::NUMERICAL,"499");
+	entity = Entity("xbox", Entity::ENT_ITEM);
+	entity.attachFeature("merchant", Feature::CATEGORY, "ms");
+	entity.attachFeature("price", Feature::NUMERICAL, "499");
 	entity.write();
-	entity = Entity("0x59373",Entity::ENT_ITEM);
-	entity.attachFeature("merchant",Feature::CATEGORY,"amazon");
-	entity.attachFeature("price",Feature::NUMERICAL,"79.99");
+	entity = Entity("0x59373", Entity::ENT_ITEM);
+	entity.attachFeature("merchant", Feature::CATEGORY, "amazon");
+	entity.attachFeature("price", Feature::NUMERICAL, "79.99");
 	entity.write();
-	entity = Entity("0xdfdfdf",Entity::ENT_ITEM);
-	entity.attachFeature("merchant",Feature::CATEGORY,"amazon");
-	entity.attachFeature("price",Feature::NUMERICAL,"79.99");
+	entity = Entity("0xdfdfdf", Entity::ENT_ITEM);
+	entity.attachFeature("merchant", Feature::CATEGORY, "amazon");
+	entity.attachFeature("price", Feature::NUMERICAL, "79.99");
 	entity.write();
-
-
 	cout << entity;
 }
 
--- src/recsys/data/Entity.h
+++ src/recsys/data/Entity.h
@@ -12,16 +12,25 @@
 #include <map>
 #include <ostream>
 #include <boost/shared_ptr.hpp>
+#include <recsys/data/json/elements.h>
+#include <recsys/data/json/reader.h>
+#include <recsys/data/json/writer.h>
 #include <cppconn/prepared_statement.h>
 #include "SQL.h"
 
 using namespace std;
 using namespace boost;
 using namespace sql;
+/// just namespace shortcut
+namespace js=json;
 
 namespace recsys {
 
+string json_to_string(js::Object& jsObj);
+js::Object string_to_json(string const& jsonStr);
+
 typedef shared_ptr<PreparedStatement> prepared_statement_ptr;
+typedef unsigned short ushort;
 
 class Entity {
 	friend ostream& operator<<(ostream&, Entity const&);
@@ -38,107 +47,69 @@ public:
 	};
 public:
 	typedef map<size_t, string> type_name_map;
+	typedef shared_ptr<Entity> entity_ptr;
+	/// retrieve the entity based on the internal id (an integer)
+	typedef map<size_t,entity_ptr> entity_ptr_map;
+	/// retrieve the entity map given entity type
+	typedef map<ushort,entity_ptr_map> type_entity_map;
+	/// keep track of the maximum id given entity type
+	typedef map<ushort,size_t> type_max_id_map;
+	/// map entity original name to internal integer index
+	typedef map<string,size_t> name_id_map;
+	typedef map<ushort,name_id_map> type_name_id_map;
+	/// internal id to entity name lookup table
+	typedef map<size_t,string> id_name_map;
+	typedef map<ushort,id_name_map> type_id_name_map;
 protected:
 	static type_name_map m_typeNameMap;
 	static SharedData m_sharedData;
+	/// store all entities in main memory
+	static type_entity_map m_type_entity_map;
+	/// store the maximum id for a given entity type
+	static type_max_id_map m_type_max_id_map;
+	/// entity name to internal id lookup table
+	static type_name_id_map m_type_name_id_map;
+	/// internal id to entity name lookup
+	static type_id_name_map m_type_id_name_map;
 protected:
+	/// whether keep all objects in memory
+	bool m_memory_mode;
 	string m_id;
 	/// an unsigned integer id used internally.
 	unsigned int m_mapped_id;
 	size_t m_type;
-	map<size_t, string> m_kvMap;
+	/// use a json object to hold all information about the entity
+	/// e.g. attributes(features) about a user or an item
+	js::Object m_json_value;
 protected:
 	unsigned int _get_max_mapped_id(bool &exist);
 public:
-	Entity(string const& id = "", size_t const& type = ENT_DEFAULT) :
+	Entity(string const& id = "", size_t const& type = ENT_DEFAULT, bool memoryMode = true) :m_memory_mode(memoryMode),
 			m_id(id),m_mapped_id(0),m_type(type) {
 	}
-	static type_name_map initTypeNameMap();
-	static SharedData initSharedData();
-	void reset() {
-		m_kvMap.clear();
-	}
-
-	inline string getId() {
+	inline string get_id() {
 		return m_id;
 	}
-
-	inline unsigned int get_mapped_id(){
+	inline size_t get_mapped_id(){
 		return m_mapped_id;
 	}
-
-	map<size_t, string> const& getKVMap() const {
-		return m_kvMap;
-	}
-
-	size_t attachFeature(string const& name, size_t const& type,
-			string const& value);
-
 	bool exist();
-	string getNameByType(size_t const& type) {
-		return m_typeNameMap[type];
-	}
-
-	void setKV(size_t const& key, string const& value) {
-		m_kvMap[key] = value;
-	}
-
-	string getValue(size_t const& key) {
-		if (m_kvMap.find(key) != m_kvMap.end()) {
-			return m_kvMap[key];
-		}
-		return string();
-	}
-	string getEntityValue() const;
 	void write();
 	void read();
 	virtual ~Entity() {
 	}
+public:
+	static type_name_map init_type_name_map();
+	static SharedData init_shared_data();
+	static entity_ptr query_by_nameAndType(string const& name, ushort const& type, bool memory_mode = true);
 };
 
 ostream& operator<<(ostream& oss, Entity const&);
-
-class Feature {
-	struct SharedData {
-		/// prepared statement
-		prepared_statement_ptr m_insertStmtPtr;
-		prepared_statement_ptr m_queryByNameStmtPtr;
-		SharedData(PreparedStatement* stmtPtr = NULL) :
-				m_insertStmtPtr(stmtPtr) {
-
-		}
-	};
-
-public:
-	enum FEATURE_TYPE {
-		NUMERICAL=1, CATEGORY=2, ORDINAL=3
-	};
-protected:
-	string m_name;
-	size_t m_type;
-	size_t m_id;
-protected:
-	static SharedData m_sharedSqlData;
-public:
-	Feature(string const& name, size_t const& type) :
-			m_name(name), m_type(type), m_id(0) {
-
-	}
-	static SharedData initSharedData();
-	bool exist();
-	string getName() {
-		return m_name;
-	}
-	size_t getType() {
-		return m_type;
-	}
-	size_t getId() {
-		return m_id;
-	}
-	void read();
-	void write();
+enum FEATURE_TYPE {
+	NUMERICAL=1, CATEGORY=2, ORDINAL=3
 };
-void test_feature();
+
+
 void test_entity();
 
 }
