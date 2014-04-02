11b3950d67300eb15210bdfb89ed3b4744718139
Qi Zhao <manazhao@gmail.com>
2014-04-01 17:38:39 -0700

    upload the debug makefiles

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -larmadillo -lthrift -lthriftz -lmysqlcppconn -lboost_system -lboost_filesystem
+
