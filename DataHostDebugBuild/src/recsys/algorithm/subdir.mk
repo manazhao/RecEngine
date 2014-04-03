966fc37ade5e959439967dbce4c59ccc2ecdadd6
Qi Zhao <manazhao@gmail.com>
2014-04-03 10:12:06 -0700

    clean the build configuration

---
+++ DataHostDebugBuild/src/recsys/algorithm/subdir.mk
@@ -0,0 +1,51 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/recsys/algorithm/FeatureObject.cpp \
+../src/recsys/algorithm/Feedback.cpp \
+../src/recsys/algorithm/HierarchicalHybridMF.cpp \
+../src/recsys/algorithm/ItemObject.cpp \
+../src/recsys/algorithm/LatentObject.cpp \
+../src/recsys/algorithm/MFImplicitModel.cpp \
+../src/recsys/algorithm/MFVB.cpp \
+../src/recsys/algorithm/RecEngine.cpp \
+../src/recsys/algorithm/UserObject.cpp \
+../src/recsys/algorithm/VLImplicitModel.cpp 
+
+OBJS += \
+./src/recsys/algorithm/FeatureObject.o \
+./src/recsys/algorithm/Feedback.o \
+./src/recsys/algorithm/HierarchicalHybridMF.o \
+./src/recsys/algorithm/ItemObject.o \
+./src/recsys/algorithm/LatentObject.o \
+./src/recsys/algorithm/MFImplicitModel.o \
+./src/recsys/algorithm/MFVB.o \
+./src/recsys/algorithm/RecEngine.o \
+./src/recsys/algorithm/UserObject.o \
+./src/recsys/algorithm/VLImplicitModel.o 
+
+CPP_DEPS += \
+./src/recsys/algorithm/FeatureObject.d \
+./src/recsys/algorithm/Feedback.d \
+./src/recsys/algorithm/HierarchicalHybridMF.d \
+./src/recsys/algorithm/ItemObject.d \
+./src/recsys/algorithm/LatentObject.d \
+./src/recsys/algorithm/MFImplicitModel.d \
+./src/recsys/algorithm/MFVB.d \
+./src/recsys/algorithm/RecEngine.d \
+./src/recsys/algorithm/UserObject.d \
+./src/recsys/algorithm/VLImplicitModel.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/recsys/algorithm/%.o: ../src/recsys/algorithm/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"/home/qzhao2/git/RecEngine/src" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
