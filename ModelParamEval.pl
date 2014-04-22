855350e0d133340b0b0744ddd1cbb6634ae45603
qizhao <manazhao@gmail.com>
2014-04-21 22:24:07 -0700

    minor change for ModelParamEval.pl

--- ModelParamEval.pl
+++ ModelParamEval.pl
@@ -12,14 +12,14 @@ use strict;
 use warnings;
 
 my @lat_dim = (5,10,20,50);
-my $max_iter = 20;
+my $max_iter = 50;
 
 -d 'result' or  mkdir 'result';
 
 foreach (@lat_dim){
 my $dim = $_;
 my $resultFile = "result/dim_$dim.txt";
-my $cmd = "./RecAlgorithmReleaseBuild/RecEngine --lat-dim=$dim --max-iter=20 >$resultFile 2>&1 ";
+my $cmd = "./RecAlgorithmReleaseBuild/RecEngine --lat-dim=$dim --max-iter=$max_iter >$resultFile 2>&1 ";
 print "$cmd\n";
 `$cmd`;
 }
