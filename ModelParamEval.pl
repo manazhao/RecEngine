175cbadf0cf8deb6c0a40627a82a96846e980ad1
qizhao <manazhao@gmail.com>
2014-04-21 22:11:51 -0700

    modified ModelParamEval.pl

--- ModelParamEval.pl
+++ ModelParamEval.pl
@@ -1,5 +1,13 @@
 #!/usr/bin/perl
 
+
+# Author: Qi Zhao (manazhao@gmail.com)
+# Date: April 21th 2014
+
+# evaluate Hierarchical Hybrid Matrix Factorization Model (HHMF) under different latent vector dimensionality
+# the result for each parameter setting is dumped to separate file.
+# usage: perl ModelParamEval.pl at the project root directory
+
 use strict;
 use warnings;
 
@@ -12,7 +20,7 @@ foreach (@lat_dim){
 my $dim = $_;
 my $resultFile = "result/dim_$dim.txt";
 my $cmd = "./RecAlgorithmReleaseBuild/RecEngine --lat-dim=$dim --max-iter=20 >$resultFile 2>&1 ";
-print "$cmd";
+print "$cmd\n";
 `$cmd`;
 }
 
