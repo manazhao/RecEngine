9bec9f99b78afb711487a2814d050b86caf4e6b9
Qi Zhao <manazhao@gmail.com>
2014-05-29 17:04:10 -0700

    nothing changed

---
+++ FeatureRegression/Amazon/ExtractFeature.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+# extract all features given an item
+#
+use strict;
+use warnings;
+
+while(<STDIN>){
+	chomp;
+	my ($entity_id, @features) = split /\,/;
+	# get all features, detect duplication
+	my %feat_map = ();
+	foreach my $feature (@features){
+		my ($fid,$val) = split /\:/, $feature;
+		exists $feat_map{$fid} and die "duplicate feature - $fid: $!";
+		print $fid . "\n";
+	}	
+}
