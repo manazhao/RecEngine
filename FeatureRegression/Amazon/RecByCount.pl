4698f8ef687169857c12055476d40bfb5f6b3755
Qi Zhao <manazhao@gmail.com>
2014-06-06 14:05:40 -0700

    rec by count done

--- FeatureRegression/Amazon/RecByCount.pl
+++ FeatureRegression/Amazon/RecByCount.pl
@@ -16,15 +16,16 @@ use DBI;
 my $user_feat_file;
 my $rating_file;
 my $dict_file;
+my $result_folder;
 
-GetOptions("dict=s" => \$dict_file,"user=s" => \$user_feat_file, "rating=s" => \$rating_file) or die $!;
+GetOptions("dict=s" => \$dict_file,"user=s" => \$user_feat_file, "rating=s" => \$rating_file, "result=s" => \$result_folder) or die $!;
 
-($dict_file && $user_feat_file && $rating_file) or die "feature dictionary file, user feature file,  and rating file must be provided: $!";
+($dict_file && $user_feat_file && $rating_file && $result_folder) or die "feature dictionary file, user feature file, rating file and result folder must be provided: $!";
 
 my %feat_map = ();
 my %user_feat_map = ();
 
--f $dict_file and -f $user_feat_file and -f $rating_file or die $!;
+-f $dict_file and -f $user_feat_file and -f $rating_file and -d $result_folder or die $!;
 open DICT_FILE, "<", $dict_file or die $!;
 open USER_FILE, "<", $user_feat_file or die $!;
 open RATING_FILE, "<", $rating_file or die $!;
@@ -39,24 +40,25 @@ close DICT_FILE;
 while(<USER_FILE>){
 	chomp;
 	my($user_id, @feats) = split /\,/;
+	$user_id = (split /_/, $user_id)[1];
 	my @feat_ids = map { (split /\:/, $_)[0] } @feats;
 	foreach my $feat(@feat_ids){
 		if(exists $feat_map{$feat}){
 			my $feat_name = $feat_map{$feat};
 			if($feat_name =~ /age/){
-				$user_feat_map{$user_id}->{"age"} = $feat;
+				# $user_feat_map{$user_id}->{"age"} = $feat_name;
 			}
 			if($feat_name =~ /gender/){
-				$user_feat_map{$user_id}->{"gender"} = $feat;
+				$user_feat_map{$user_id}->{"gender"} = $feat_name;
 			}
 		}
 	}
 }
 close USER_FILE;
 
-my $dsn = "DBI:mysql:database=amazon; host=localhost";
-my $dbh = DBI->connect($dsn,"root",'StU8e@uh');
-my $sth = $dbh->prepare("INSERT INTO rating_user (user_id,item_id,age,gender) VALUES (?, ?, ?, ?)");
+
+my %filter_criteria_map = ();
+
 while(<RATING_FILE>){
 	chomp;
 	my $json_obj = decode_json($_);
@@ -65,10 +67,24 @@ while(<RATING_FILE>){
 	my $rating = $json_obj->{"r"};
 	my $age = $user_feat_map{$uid}->{"age"};
 	my $gender = $user_feat_map{$uid}->{"gender"};
-	$sth->execute($uid,$iid,$age,$gender);
+	# $age = "" unless $age;
+	$gender = "" unless $gender;
+	my $criteria = $gender;
+	next if $criteria eq "" or $criteria =~ /miss/;
+	$filter_criteria_map{$criteria}->{$iid} ++;
 }
-$sth->finish();
-$dbh->disconnect();
+
+while(my($criteria,$item_cnt_map) = each %filter_criteria_map){
+	# sort by cnt
+	my @sort_items = sort {$item_cnt_map->{$b} <=> $item_cnt_map->{$a} } keys %$item_cnt_map;
+	my $rec_file = $result_folder . "/$criteria.csv";
+	print $rec_file . "\n";
+	open REC_FILE, ">", $rec_file or die $!;
+	my $lines = join("\n", (map {join(",",($_,$item_cnt_map->{$_}))} @sort_items));
+	print REC_FILE $lines;
+	close REC_FILE;
+}
+
 close RATING_FILE;
 
 
